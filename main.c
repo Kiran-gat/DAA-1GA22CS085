34c0bdde7a463595840662b81b827ea82e8eb521
Kiran-gat <blgrkiru@gmail.com>
2024-07-30 15:41:25 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,44 +1,86 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-void dfs(int a[10][10],int n,int visited[10],int source)
+
+void Dijkstra(int cost[10][10], int n,int source)
 {
-    int i;
-   visited[source]=1;
-   for(i=1;i<=n;i++)
-   {
-       if(a[source][i]==1 && visited[i]==0)
-        dfs(a,n,visited,i);
-   }
+
+
+int i,visited[10],u,v,count=1,min,dist[10];
+for(i=1;i<=n;i++)
+{
+
+
+    visited[i]=0;
+    dist[i]=cost[source][i];
 }
+visited[source]=1;
+dist[source]=0;
+
+while(count<=n)
+{
+
+    min=999;
+    for(i=1;i<=n;i++)
+    {
+
+
+
+            if(visited[i]==0&&dist[i]<min)
+            {
+
+                    min=dist[i];
+                    u=i;
+
+
+                }
+            }
+            visited[u]=1;
+            count++;
+
+        for(i=1;i<=n;i++)
+        {
+            if(dist[i]>dist[u]+cost[u][i])
+                dist[i]=dist[u]+cost[u][i];
+        }
+
+    }
+
+
+
+        printf("\n shortest path from%d\n",source);
+        for(i=1;i<=n;i++)
+            printf("\n %d to %d is %d",source,i,dist[i]);
+
+    }
+
+
+
+
+
 
 int main()
 {
-   int n,a[10][10],i,j,visited[10],count=0;
-   printf("\n read no of nodes:");
-   scanf("%d",&n);
-   printf("\n read adjacency matrix:");
-   for(i=1;i<=n;i++)
-   {
-       for(j=1;j<=n;j++)
-        scanf("%d",&a[i][j]);
-   }
+    int n,cost[10][10],i,j,source;
+    printf("\n Read number of nodes:");
+    scanf("%d",&n);
+    printf("\n Read source:");
+    scanf("%d",&source);
+    printf("\n Read cost matrix\n");
+    for(i=1;i<=n;i++)
+    {
 
-   for(i=1;i<=n;i++)
-    visited[i]=0;
-   for(i=1;i<=n;i++)
-   {
-       if(visited[i]==0)
-       {
-           dfs(a,n,visited,i);
-           count++;
-       }
+        for(j=1;j<=n;j++)
+        {
 
-   }
+            scanf("%d",&cost[i][j]);
+            if(cost[i][j]==0)
+                cost[i][j]=999;
 
-   if(count==1)
-    printf("\n graph connected ,%d component",count);
-    else
-    printf("\n graph not connected,%d component",count);
+
+
+        }
+
+    }
+     Dijkstra(cost,n,source);
     return 0;
 }
