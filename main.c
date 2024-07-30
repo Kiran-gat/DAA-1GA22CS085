44e84ea6094be77616b3e2fa8d6d15ea42990c03
Kiran-gat <blgrkiru@gmail.com>
2024-07-30 15:44:24 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -2,57 +2,70 @@
 #include <stdlib.h>
 #define size 100
 int count;
-void merge(int A[size],int l,int m,int r)
-{
 
-    int i,j,k,B[size];
-    i=l;
-    j=m+1;
-    k=l;
-    while(i<=m && j<=r)
-    {
-        count++;
-        if(A[i]<A[j])
-            B[k++]=A[i++];
-        else
-            B[k++]=A[j++];
+int partition(int A[size],int l,int r){
+
+ int i,j,pivot,temp;
+ pivot=A[l];
+ i=l+1;
+ j=r;
+ while(1){
+
+    while(pivot>=A[i]&& i<=r)
+    {  count++;
+       i++;
+    }
+
+    while(pivot<A[j])
+    {    count++;
+        j--;
 
     }
-    while(i<=m)
-        B[k++]=A[i++];
-    while(j<=r)
-        B[k++]=A[j++];
-    for(i=l;i<=r;i++)
-        A[i]=B[i];
+   count++;
+    if(i<j)
+    {
+
+        temp=A[i];
+        A[i]=A[j];
+        A[j]=temp;
+
+    }
+    else{
+
+        temp=A[j];
+        A[j]=A[l];
+        A[l]=temp;
+        return j;
+    }
+ }
+
 }
-void mergesort(int A[size],int l,int r)
-{
+void quicksort(int A[size],int l,int r){
 
+   int s;
+   if(l<r)
+   {
+       s=partition(A,l,r);
+       quicksort(A,l,s-1);
+       quicksort(A,s+1,r);
 
-    int m;
-    if(l<r)
-    {
+   }
 
-        m=(l+r)/2;
-        mergesort(A,l,m);
-        mergesort(A,m+1,r);
-        merge(A,l,m,r);
-
-    }
 }
 
 int main()
-{   int i,n,A[size];
+{
+    int i,n,A[size];
     printf("enter the size:\n");
     scanf("%d",&n);
     printf("enter the elements:\n");
-    for(i=0;i<=n-1;i++)
+    for(i=0;i<n;i++)
         scanf("%d",&A[i]);
-    mergesort(A,0,n-1);
-    printf("sorted array");
-    for(i=0;i<=n-1;i++)
-        printf("%d",A[i]);
-    printf("the total no comparison %d\n",count);
+    quicksort(A,0,n-1);
 
+    printf("sorted array\n");
+    for(i=0;i<n;i++)
+        printf("%d",A[i]);
+    printf("th comparison %d",count);
 
 }
